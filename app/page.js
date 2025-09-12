201b07d4e46512d8370bcbfe76763cacda38a8d6
Ahmed Salman <159709100+sastiservices@users.noreply.github.com>
2025-09-12 10:05:46 -0700

    ok

--- app/page.js
+++ app/page.js
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline mt-[5rem] text-center">Home</h1>
+    <h1 className="text-3xl font-bold underline mt-[5rem] text-center">Ahmed Salman Portfolios 2025</h1>
   );
 }
