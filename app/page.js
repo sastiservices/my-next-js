34e56268dd1457e23873d07b55592c0e84cd670b
Ahmed Salman <salman.sastiservices@gmail.com>
2025-09-12 10:33:22 -0700

    Merge remote changes into local project

--- app/page.js
+++ app/page.js
@@ -2,6 +2,43 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline mt-[5rem] text-center">Home</h1>
+    <h1 className="text-3xl font-bold underline mt-[5rem] text-center">Ahmed Salman Portolio 2025dfslkfjslfkdfjsdklsdjdlkdsljsddjdldsksaddkaudjiodwadujaidaujdidausidsduidasudiasduasidudiudasiduidasudiudaiduids sdsdsddsddsadsd
+        sdlkasjdlkasjdlkdkdljdlakdadkladalkdasjdlaskd
+        dsadasdasdasdmndmsdkdd
+        dmasdaslkdjdalskdaslda
+        dsakdsjdlkdjasdjdsd
+        sdkasjdslkddlkdjaslkds
+        asdkasjdaslkdjaskdldajdalskjdlkda
+        skdasdjkada\sdas\dadaddod
+        asdkasdkladjaskdjddas
+        asdajskdakldjasdklasdjkldas
+        sdaskjdasdkjadasdja
+        adkajdajskdadaksdhadjakhaskjdasdkjajkashasdkjhajsdajdsjdajdkasdkja
+   dasdasdkjadadjdkkjsdasdas
+asdaskjdakdjkdajd
+dasddaskldjdkldjlkdaklasdkdddss
+
+   sdskdjkjdasdjkj
+   dsakdlasldka
+   ldkasjdklasjdddkl
+   dsadkasjdssadkllaskdjsalkasds
+   asdasdlkjdlaskddjdkljddklaskasdk
+   dasdasdkjadadjdkkjsdasdas
+   dskasjdadkadjkajd
+   zsdsadsdsdasddsadd
+   dsadsdsad
+   dawkdjaskddashahdadhahasjkd
+   kDSAJDLASDASDJLDALDAKSDJAS
+   lorem2
+   
+erereewrwwrw
+werkwerjwekrjklwerjerklwerklrklwerrewrweelrlwr
+erekrwejrkjerklwerwelkrjekrrejrkwejrewr
+kfdjsakflsdjfksdlfjdsfklsdfjkldsfjsdfsdlfkdfjsdlkfsdjkljfsd
+sdflksdjfkslfsjfsd
+   daskdajaskdajsdasldkjdldjadasjdlad
+   
+   sdasdkjdsajdskaj00sdfkdjfkfjaskdakdasddjkjdakasjdkdsad
+    </h1>
   );
 }
