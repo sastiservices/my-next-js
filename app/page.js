e685c2bf6b803fd8b94a7fd4904b8ea818990df0
Ahmed Salman <salman.sastiservices@gmail.com>
2025-09-12 10:50:59 -0700

    update

--- app/page.js
+++ app/page.js
@@ -1,64 +1,9 @@
 import Image from "next/image";
 export default function Home() {
   return (
-<<<<<<< HEAD
-    <h1 className="text-3xl font-bold underline mt-[5rem] text-center">Ahmed Salman Portolio 2025dfslkfjslfkdfjsdklsdjdlkdsljsddjdldsksaddkaudjiodwadujaidaujdidausidsduidasudiasduasidudiudasiduidasudiudaiduids sdsdsddsddsadsd
-        sdlkasjdlkasjdlkdkdljdlakdadkladalkdasjdlaskd
+      <h5>
+        Hello nafsiati pana
+      </h5>
 
-mkjcfjjkzhjkdkjsadkj        dsadasdasdasdmndmsdkdd
-        dmasdaslkdjdalskdaslda
-        dsakdsjdlkdjasdjdsd
-        sdkasjdslkddlkdjaslkds
-        dasldasdklasdkada
-        sdasdkas
-        dsakdadjkajdsdjdaa
-        adskadjask
-        xcfdsffsdfsfsdsfs
-        sdasldkddkldas
-        asdklasdaskddkldsaa
-        dfdddfgdgdfgdgdfdfdg
-        fghgdfgdfdfggdffgd
-        asdkadadlkjdasldkasdkldaskldkaldjadklajdasklasjldjasdkladjkasd
-        asdkadjakdjadlkadaskdas
-        adklasjdkldskdjadakld
-        adkasldaskdlajdakdjakslda
-        sdalskdaldkdadasjdasldladask
-        daskldaslkdasjdlasdkasdjasdljlasdjasldk
-        asdaskldjadlkajdalkadklasdjsa
-        asdkasjdaslkdjaskdldajdalskjdlkda
-        skdasdjkada\sdas\dadaddod
-        asdkasdkladjaskdjddas
-        asdajskdakldjasdklasdjkldas
-        sdaskjdasdkjadasdja
-        adkajdajskdadaksdhadjakhaskjdasdkjajkashasdkjhajsdajdsjdajdkasdkja
-   dasdasdkjadadjdkkjsdasdas
-asdaskjdakdjkdajd
-dasddaskldjdkldjlkdaklasdkdddss
-
-   sdskdjkjdasdjkj
-   dsakdlasldka
-   ldkasjdklasjdddkl
-   dsadkasjdssadkllaskdjsalkasds
-   asdasdlkjdlaskddjdkljddklaskasdk
-   dasdasdkjadadjdkkjsdasdas
-   dskasjdadkadjkajd
-   zsdsadsdsdasddsadd
-   dsadsdsad
-   dawkdjaskddashahdadhahasjkd
-   kDSAJDLASDASDJLDALDAKSDJAS
-   lorem2
-   
-erereewrwwrw
-werkwerjwekrjklwerjerklwerklrklwerrewrweelrlwr
-erekrwejrkjerklwerwelkrjekrrejrkwejrewr
-kfdjsakflsdjfksdlfjdsfklsdfjkldsfjsdfsdlfkdfjsdlkfsdjkljfsd
-sdflksdjfkslfsjfsd
-   daskdajaskdajsdasldkjdldjadasjdlad
-   
-   sdasdkjdsajdskaj00sdfkdjfkfjaskdakdasddjkjdakasjdkdsad
-    </h1>
-=======
-    <h1 className="text-3xl font-bold underline mt-[5rem] text-center">Ahmed Salman Portfolios 2025</h1>
->>>>>>> 201b07d4e46512d8370bcbfe76763cacda38a8d6
   );
 }
