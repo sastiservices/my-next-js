0d18b8682fac9b58d4f13c6f43ab3ad103543b28
Ahmed Salman <salman.sastiservices@gmail.com>
2025-09-15 10:54:19 -0700

    update

--- app/page.js
+++ app/page.js
@@ -1,14 +1,10 @@
 import Image from "next/image";
 export default function Home() {
   return (
-      <h5>
-        Hello nafsiati pana
-        dsfgdfgdfgddfgdfgdf lis hhghgfhfhfhfhfhfhfghfghfggfhhfhfggh okkkkkkkkkkkkkk
-      
-      okmok   okok ok okokokokokkokkokokokokokok
-          bhai fine hai kia
-          Hai nh
-      </h5>
+    <div className="w-[100%] px-[20px] h-[100dvh] flex justify-center items-center flex-col">
+      <h5 className="text-[1.1rem] text-center"> Taking a short break to improve your experience.</h5>
+      <a className="block text-center mt-[30px] dowmnlaod-resume" href="/AHMED-SALMAN.pdf" download>Download Resume</a>
+    </div>
 
   );
 }
